c1361701f0606c1ea8641bc09d704aaedc8e2845
Paras Singh <50452909+ParasSingh-siparas@users.noreply.github.com>
2020-05-01 06:08:35 -0400

    Made Repository Model
    Fetched Response, saved it and displayed Response data

--- PartyInvites/PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -32,8 +32,16 @@ namespace PartyInvites.Controllers
         [HttpPost]
         public IActionResult RsvpForm(GuestResponse response)
         {
+            Repository.AddResponse(response);
+
             ViewBag.respond = response.Attend == true ? "Thank You for accepting the invitation :) meet you at the party, " + response.Name:"Ohh, we will miss you at the party. Do let us know if you change your plans, " + response.Name;
             return View("ThankYou");
+        }
+
+        public IActionResult ListResponses()
+        {
+            //var attendees = Repository.Responses.Where(r => r.Attend == true);
+            return View(Repository.Responses.Where(r => r.Attend == true));
         }
 
         //public IActionResult Privacy()
---
+++ PartyInvites/PartyInvites/Models/Repository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyInvites.Models
+{
+    public static class Repository
+    {
+        private static List<GuestResponse> _responses = new List<GuestResponse>();
+
+        public static void AddResponse(GuestResponse response)
+        {
+            _responses.Add(response);
+        }
+
+        public static IEnumerable<GuestResponse> Responses
+        {
+            get { return _responses; }
+        }
+    }
+}
